3050204d64b457556380d5c4b8d8549fef36186b
KingDarBoja <stevenbojato04@gmail.com>
2020-07-13 18:41:48 -0500

    chore: add scully

---
+++ scully.kingdarboja-io.config.ts
@@ -0,0 +1,8 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'kingdarboja-io',
+  outDir: './dist/static',
+  routes: {},
+};
