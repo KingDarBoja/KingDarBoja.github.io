806f9f366ee1eed6e807f5fb2ac7075d30a7b3f7
KingDarBoja <stevenbojato04@gmail.com>
2020-08-09 17:04:30 -0500

    chore: setup colour palette on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      oxford: {
+        default: "#14213D",
+        dark: "#00132D"
+      },
+      orange: {
+        default: "#FCA311"
+      },
+      platinum: {
+        default: "#E5E5E5"
+      }
+    },
   },
   variants: {},
   plugins: [],
