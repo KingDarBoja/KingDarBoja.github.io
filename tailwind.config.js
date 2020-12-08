3ced001741b2f9408604083dcf789e3a74ec2251
KingDarBoja <stevenbojato04@gmail.com>
2020-12-08 16:34:15 -0500

    chore: replace ng-tailwindcss for ngneat/tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,28 @@
-module.exports = {
-  purge: [],
-  theme: {
-    colors: {
-      transparent: "transparent",
-      oxford: {
-        default: "#14213D",
-        dark: "#00132D"
-      },
-      orange: {
-        default: "#FCA311"
-      },
-      platinum: {
-        default: "#E5E5E5"
-      }
+module.exports = (isProd) => ({
+    prefix: '',
+    purge: {
+      enabled: isProd,
+      content: ['**/*.html', '**/*.ts']
     },
-  },
-  variants: {},
-  plugins: [],
-}
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      // extend: {},
+      colors: {
+        transparent: "transparent",
+        oxford: {
+          DEFAULT: "#14213D",
+          dark: "#00132D"
+        },
+        orange: {
+          DEFAULT: "#FCA311"
+        },
+        platinum: {
+          DEFAULT: "#E5E5E5"
+        }
+      },
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+});
